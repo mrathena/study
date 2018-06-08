172a66445872486a378294684e09cca625fca3d7
hushirui <747779761@qq.com>
2018-06-08 11:08:35 +0800

    .

---
+++ src/main/java/com/mrathena/study/concurrent/locks/LockAndConditionTest.java
@@ -0,0 +1,10 @@
+package com.mrathena.study.concurrent.locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockAndConditionTest {
+
+	Lock lock = new ReentrantLock();
+
+}
